0bd83a9efe5e6299e1ae1ce75b5b6fd5bf0bea58
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2026-08-09 22:20:54 +0530

    added data models

--- pulse/admin.py
+++ pulse/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import NotificationLog, PriceAlert, UserProfile
+
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    fk_name = 'user'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [UserProfileInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role', 'created_at')
+    list_filter = ('role',)
+    search_fields = ('user__username', 'user__email')
+
+
+@admin.register(PriceAlert)
+class PriceAlertAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+        'origin',
+        'destination',
+        'threshold_price',
+        'status',
+        'created_at',
+    )
+    list_filter = ('status', 'origin', 'destination')
+    search_fields = ('user__username', 'origin', 'destination')
+
+
+@admin.register(NotificationLog)
+class NotificationLogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'alert', 'triggered_price', 'notified_at')
+    search_fields = ('message', 'alert__origin', 'alert__destination')
--- pulse/apps.py
+++ pulse/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class PulseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pulse'
+
+    def ready(self):
+        # Wire post_save(User) → UserProfile creation.
+        import pulse.signals  # noqa: F401
